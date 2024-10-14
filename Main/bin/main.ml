4f11ed2bfa458b29bf415866a6cc0f524e723817
Boris D. <boris.dob@gmail.com>
2024-10-14 11:41:58 -0700

    Refactor.

--- Main/bin/main.ml
+++ Main/bin/main.ml
@@ -2,9 +2,8 @@ open AppKit
 open Runtime
 
 let main () =
-  let delegate = AppDelegate.define ()
+  let _ = AppDelegate.define ()
   and _ = ViewController.define ()
-  and app = NSApplication.self |> NSApplicationClass.sharedApplication
   and argc = Array.length Sys.argv
   and argv =
     Sys.argv
@@ -12,12 +11,6 @@ let main () =
     |> Objc.(CArray.of_list string)
     |> Objc.CArray.start
   in
-  app
-  |> NSApplication.setActivationPolicy _NSApplicationActivationPolicyRegular
-  |> ignore;
-  app |> NSApplication.setDelegate delegate;
-  app |> NSApplication.activateIgnoringOtherApps true;
-
   _NSApplicationMain argc argv |> exit
 
 let () = main ()
